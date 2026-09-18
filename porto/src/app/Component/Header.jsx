f0732436bc79a8f0d07dfea88cb4bdc28b3b92d0
VincentAsP <vincentiusaxelpradono@gmail.com>
2026-09-18 13:04:23 +0700

    ganti header

--- porto/src/app/Component/Header.jsx
+++ porto/src/app/Component/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="fixed top-20 left-50 z-50 px-6 py-4 bg-[#f5f5f5] rounded-2xl">
+    <header className="absolute top-20 left-50 z-50 px-6 py-4 bg-[#f5f5f5] rounded-2xl">
         <div className="text-xl font-light text-29">
           <nav className="flex gap-70">
             <a href="/" className="inline-flex items-center justify-center gap-1.5 ">Home <span className="w-2.5 h-2.5 rounded-full bg-current"></span> </a>
